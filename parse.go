03f02c89eb6281d3b65afd234989575dbad34d95
nbari <nbari@tequila.io>
2016-11-06 01:11:47 +0100

    endfor

--- parse.go
+++ parse.go
@@ -3,6 +3,8 @@ package parser
 import (
 	"bufio"
 	"bytes"
+	"fmt"
+	"strings"
 )
 
 // Parse parse the template
@@ -12,13 +14,12 @@ func (p *Parser) Parse() (string, error) {
 		buf    bytes.Buffer
 		useBuf bool
 		//	err error
-		inLoop bool
-		//inLoopBody  bool
-		//lineBuffer  []string
-		lineNum int = 1
-	//		placeHolder string
-	//		position int
-	//		variable string
+		inLoop       bool
+		inLoopBody   bool
+		lineNum      int = 1
+		placeHolder  string
+		position     int
+		loopVariable string
 	)
 
 	scanner := bufio.NewScanner(p.template)
@@ -26,16 +27,42 @@ func (p *Parser) Parse() (string, error) {
 
 	for scanner.Scan() {
 		c := scanner.Text()
+		// handle for loop
 		switch c {
 		case "\n":
 			lineNum++
+			if inLoopBody {
+				line := buf.String()
+				buf.Reset()
+				if strings.Contains(line, placeHolder) {
+					// loop Body
+					if k, ok := p.Variables[loopVariable]; ok {
+						for _, v := range k.([]interface{}) {
+							ln := fmt.Sprintf("%s\n",
+								strings.Replace(line,
+									placeHolder,
+									fmt.Sprintf("%v", v),
+									-1),
+							)
+							buf.WriteString(ln)
+						}
+					}
+				}
+			} else {
+				buf.WriteString(c)
+			}
 			useBuf = false
-			buf.WriteString(c)
 			out.WriteString(buf.String())
 			buf.Reset()
 		case p.Delimeter:
 			buf.WriteString(c)
-			if useBuf && len(buf.String()) == 2 {
+			if useBuf && position == 2 {
+				position = 0
+				loopVariable = strings.Replace(buf.String(), p.Delimeter, "", -1)
+				inLoopBody = true
+				buf.Reset()
+				continue
+			} else if useBuf && len(buf.String()) == 2 {
 				useBuf = false
 				out.WriteString(buf.String())
 				buf.Reset()
@@ -48,18 +75,38 @@ func (p *Parser) Parse() (string, error) {
 					return "", err
 				}
 				out.WriteString(str)
+			} else if buf.String() == "$endfor$" {
+				fmt.Printf("buf.String() = %+v\n", buf.String())
 			} else {
 				useBuf = true
 			}
 		default:
-			if useBuf {
+			if inLoopBody {
+				buf.WriteString(c)
+			} else if useBuf {
 				buf.WriteString(c)
 				if c == " " {
 					switch buf.String() {
 					case "$for ":
 						inLoop = true
+						buf.Reset()
 					default:
 						if inLoop {
+							switch position {
+							case 0:
+								placeHolder = fmt.Sprintf("%s%s%s",
+									p.Delimeter,
+									strings.TrimSpace(buf.String()),
+									p.Delimeter)
+							case 1:
+								if buf.String() != "in " {
+									return "", fmt.Errorf("Error parsing template, please verify the syntax on line %d", lineNum)
+								}
+							case 2:
+								return "", fmt.Errorf("Error parsing template, please verify the syntax on line %d", lineNum)
+							}
+							buf.Reset()
+							position++
 						} else {
 							useBuf = false
 							out.WriteString(buf.String())
@@ -72,6 +119,6 @@ func (p *Parser) Parse() (string, error) {
 			}
 		}
 	}
-
+	fmt.Println(placeHolder, loopVariable)
 	return out.String(), nil
 }
